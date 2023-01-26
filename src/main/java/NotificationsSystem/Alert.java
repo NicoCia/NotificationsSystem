46af29c65d5ed7eaa9938ed183c2c897ad117fa9
NicoCia <nicocia7@gmail.com>
2023-01-26 06:06:10 -0300

    implementacion setters y getters de alerta y user + recepcion y lectura de alertas por los usuarios

--- src/main/java/NotificationsSystem/Alert.java
+++ src/main/java/NotificationsSystem/Alert.java
@@ -1,5 +1,38 @@
 package NotificationsSystem;
 
 public class Alert {
+    private String topic;
+    private String type;
+    private String text;
+    private String alertedUsers;
+    private Boolean ExpirationFlag;
+
+    public Alert(String topic, String type, String text, String alertedUsers) {
+        this.topic = topic;
+        this.type = type;
+        this.text = text;
+        this.alertedUsers = alertedUsers;
+        ExpirationFlag = false;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAlertedUsers() {
+        return alertedUsers;
+    }
+
+    public void setExpirationFlag(Boolean ExpirationFlag) {
+        this.ExpirationFlag = ExpirationFlag;
+    }
     
 }
--- src/main/java/NotificationsSystem/User.java
+++ src/main/java/NotificationsSystem/User.java
@@ -1,5 +1,48 @@
 package NotificationsSystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String name;
+    private List<Alert> unreadUrgentAlerts;
+    private List<Alert> unreadInformativeAlerts;
+    
+
+    public User(String name) {
+        this.name = name;
+        unreadUrgentAlerts = new ArrayList<Alert> ();
+        unreadInformativeAlerts = new ArrayList<Alert> ();
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+
+    public List<Alert> getUnreadUrgentAlerts() {
+        return unreadUrgentAlerts;
+    }
+
+
+    public List<Alert> getUnreadInformativeAlerts() {
+        return unreadInformativeAlerts; 
+    }
+
+    public void addNewUrgentAlert(Alert newUrgentAlert){
+        unreadUrgentAlerts.add(0,newUrgentAlert);
+    }
+
+    public void addNewInformativeAlert(Alert newInformativeAlert){
+        unreadInformativeAlerts.add(0,newInformativeAlert);
+    }
+
+    public void markAlertAsRead(Alert readAlert){
+        if(readAlert.getType().equals("urgente")) 
+            unreadUrgentAlerts.remove(readAlert);
+
+        else unreadInformativeAlerts.remove(readAlert);
+    }
     
 }
