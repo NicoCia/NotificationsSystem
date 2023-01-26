44b939cfd34864e60ae2eda31ecd1bc6607fccd7
NicoCia <nicocia7@gmail.com>
2023-01-26 08:05:27 -0300

    implementacion de registro de usuarios y topicos + envio de alertas por topico o a usuario especifico

--- src/main/java/NotificationsSystem/Notifier.java
+++ src/main/java/NotificationsSystem/Notifier.java
@@ -1,5 +1,63 @@
 package NotificationsSystem;
 
+// import java.util.ArrayList;
+import java.util.HashMap;
+// import java.util.List;
+
 public class Notifier {
+    private HashMap<String, User> registeredUsersMap;
+    private HashMap<String, Subject> topicsObserversMap;
+    private Alert lastAlert; 
+    
+    public Notifier() {
+        registeredUsersMap = new HashMap<String, User>();
+        topicsObserversMap = new HashMap<String, Subject>();
+        
+    }
+
+    public void createNewNotification(){
+        // TODO implement method
+    }
+
+    public Boolean addNewTopic(String topic){
+        if(!topicsObserversMap.containsKey(topic)){
+            NotificationsDispatcher newNotificationDispatcherForNewTopic = new NotificationsDispatcher(Notifier.this);
+            topicsObserversMap.put(topic, newNotificationDispatcherForNewTopic);
+            return true;
+        }
+        else return false;
+    }
+
+    public Boolean addNewUser(User user){
+
+        if(!registeredUsersMap.containsKey(user.getName()))
+        {
+            registeredUsersMap.put(user.getName(), user);
+            return true;
+        }
+        else return false;
+    }
+
+    public Boolean suscribeUserToTopic (String userName, String topic){
+        
+        if(registeredUsersMap.containsKey(userName) && topicsObserversMap.containsKey(topic)){
+            User suscriberUser = registeredUsersMap.get(userName);
+            Subject notificationDispatcherOfTopic = topicsObserversMap.get(topic);
+            notificationDispatcherOfTopic.registerObserver(suscriberUser);
+            return true;
+        }
+        else return false;
+    }
+
+    public Alert getLastAlert() {
+        return lastAlert;
+    }
+
+    public User getUserByName(String userName){
+        if(registeredUsersMap.containsKey(userName)){
+            return registeredUsersMap.get(userName);
+        }
+        else return null;
+    }
     
 }
--- src/main/java/NotificationsSystem/Register.java
+++ src/main/java/NotificationsSystem/Register.java
@@ -1,5 +1,27 @@
 package NotificationsSystem;
 
 public class Register {
-    
+    private Notifier miNotifier;
+
+    public Register(Notifier miNotifier) {
+        this.miNotifier = miNotifier;
+    }
+
+    public void createNewRegistration(){
+        // TODO implement method
+    }
+
+    public Boolean registerUser(String newUserName){
+        User newUser = new User(newUserName, miNotifier);
+
+        return miNotifier.addNewUser(newUser);
+    }
+
+    public Boolean registerNewTopic(String newTopString){
+        return miNotifier.addNewTopic(newTopString);
+    }
+
+    public Boolean registerUserToTopic(String suscriberUserName, String topic){
+        return miNotifier.suscribeUserToTopic(suscriberUserName, topic);
+    }
 }
